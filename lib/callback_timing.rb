fa34783df986998c77e888470a1363c09a7341dc
Matt Gleeson <matt@tres.rwc.cubetree.com>
2009-11-06 14:54:33 -0800

    only run timing if callback is evaluated

--- lib/callback_timing.rb
+++ lib/callback_timing.rb
@@ -1,15 +1,15 @@
 module ActiveSupport
   module Callbacks
     class Callback
-      def call_with_timing(*args)
+      def evaluate_method_with_timing(method, *args, &block)
         rv = nil
         real_time = Benchmark.realtime do
-          rv = call_without_timing(*args)
+          rv = evaluate_method_without_timing(method, *args, &block)
         end
         RAILS_DEFAULT_LOGGER.info("Callback Timing (#{sprintf('%.1fms', real_time*1000)}) #{method.to_s}")
         return rv
       end
-      alias_method_chain :call, :timing
+      alias_method_chain :evaluate_method, :timing
     end
   end
 end
